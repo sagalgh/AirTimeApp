f58d388d9aa40b5b4788973ced5bcca1c2d11b92
Ansaar Ahmed <ansaar.ahmed245@gmail.com>
2022-01-15 21:39:26 -0500

    google maps and autocomplete search, dummy pages for home, chat, flight, tsa

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Flight from "./components/Flight";
+import Map from "./components/Map";
+import Tsa from "./components/Tsa";
+import Chat from "./components/Chat";
+
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/map" element={<Map />} />
+      <Route path="/flight" element={<Flight />} />
+      <Route path="/tsa" element={<Tsa />}></Route>
+      <Route path="/chat" element={<Chat />}></Route>
+    </Routes>
+    <Footer />
+  </Router>,
+
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
