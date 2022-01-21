7d0c5a6442537c4bb852dc0800c855acc9746cbd
Ansaar Ahmed <ansaar.ahmed245@gmail.com>
2022-01-20 20:54:44 -0500

    unique styling for map

---
+++ client/src/components/Map/mapStyles.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    featureType: 'all',
+    elementType: 'all',
+    stylers: [
+      {
+        saturation: '32',
+      },
+      {
+        lightness: '-3',
+      },
+      {
+        visibility: 'on',
+      },
+      {
+        weight: '1.18',
+      },
+    ],
+  },
+];
