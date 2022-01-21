97c49b30cd3ee265048a4bb0d0bd0b6fa7bb354c
Ansaar Ahmed <ansaar.ahmed245@gmail.com>
2022-01-21 10:58:45 -0500

    fixed imports

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,84 +1,112 @@
-import {useState} from 'react';
-import "/Users/sagalafrah/lighthouse/w11/AirTimeApp/client/src/home.css"
+import { useState } from 'react';
+import '../home.css';
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
-import useStyles from "./style"
+import useStyles from './style';
 
 function Home() {
   const [autocomplete, setAutocomplete] = useState(null);
   const [coords, setCoords] = useState({});
   const classes = useStyles();
 
-  
   const onLoad = (autoC) => setAutocomplete(autoC);
 
   const onPlaceChanged = () => {
-    const lat = autocomplete.getPlace().geometry.location.lat()
-    const lng = autocomplete.getPlace().geometry.location.lng()
-    console.log("lat and lng", lat, lng);
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+    console.log('lat and lng', lat, lng);
 
     setCoords({ lat, lng });
   };
-  
+
   return (
-    <div className="container">
-    <div id="splash">
-    <div className="anim">
-      <div id="loader">
-        <svg version="1.1" width="60px" height="70px" viewBox="0 0 60 70">
-          <defs>
-            <filter id="f1" x={0} y={0}>
-              <feGaussianBlur in="SourceGraphic" stdDeviation={2} />
-            </filter>
-          </defs>
-          <g id="airplane">
-            <path fill="#000" d="M0.677,20.977l4.355,1.631c0.281,0.104,0.579,0.162,0.88,0.16l9.76-0.004L30.46,41.58c0.27,0.34,0.679,0.545,1.112,0.541
+    <div className='container'>
+      <div id='splash'>
+        <div className='anim'>
+          <div id='loader'>
+            <svg version='1.1' width='60px' height='70px' viewBox='0 0 60 70'>
+              <defs>
+                <filter id='f1' x={0} y={0}>
+                  <feGaussianBlur in='SourceGraphic' stdDeviation={2} />
+                </filter>
+              </defs>
+              <g id='airplane'>
+                <path
+                  fill='#000'
+                  d='M0.677,20.977l4.355,1.631c0.281,0.104,0.579,0.162,0.88,0.16l9.76-0.004L30.46,41.58c0.27,0.34,0.679,0.545,1.112,0.541
       h1.87c0.992,0,1.676-0.992,1.322-1.918l-6.643-17.439l6.914,0.002l6.038,6.037c0.265,0.266,0.624,0.412,0.999,0.418l1.013-0.004
       c1.004-0.002,1.684-1.012,1.312-1.938l-2.911-7.277l2.912-7.278c0.372-0.928-0.313-1.941-1.313-1.938h1.017
       c-0.375,0-0.732,0.15-0.996,0.414l-6.039,6.039h-6.915l6.646-17.443c0.354-0.926-0.33-1.916-1.321-1.914l-1.87-0.004
       c-0.439,0.004-0.843,0.203-1.112,0.543L15.677,17.24l-9.765-0.002c-0.3,0.002-0.597,0.055-0.879,0.16L0.678,19.03
-      C-0.225,19.36-0.228,20.637,0.677,20.977z" transform="translate(44,0) rotate(90 0 0)" />
-          </g>
-          <g id="shadow" transform="scale(.9)">
-            <path fill="#000" fillOpacity="0.3" d="M0.677,20.977l4.355,1.631c0.281,0.104,0.579,0.162,0.88,0.16l9.76-0.004L30.46,41.58c0.27,0.34,0.679,0.545,1.112,0.541
+      C-0.225,19.36-0.228,20.637,0.677,20.977z'
+                  transform='translate(44,0) rotate(90 0 0)'
+                />
+              </g>
+              <g id='shadow' transform='scale(.9)'>
+                <path
+                  fill='#000'
+                  fillOpacity='0.3'
+                  d='M0.677,20.977l4.355,1.631c0.281,0.104,0.579,0.162,0.88,0.16l9.76-0.004L30.46,41.58c0.27,0.34,0.679,0.545,1.112,0.541
       h1.87c0.992,0,1.676-0.992,1.322-1.918l-6.643-17.439l6.914,0.002l6.038,6.037c0.265,0.266,0.624,0.412,0.999,0.418l1.013-0.004
       c1.004-0.002,1.684-1.012,1.312-1.938l-2.911-7.277l2.912-7.278c0.372-0.928-0.313-1.941-1.313-1.938h1.017
       c-0.375,0-0.732,0.15-0.996,0.414l-6.039,6.039h-6.915l6.646-17.443c0.354-0.926-0.33-1.916-1.321-1.914l-1.87-0.004
       c-0.439,0.004-0.843,0.203-1.112,0.543L15.677,17.24l-9.765-0.002c-0.3,0.002-0.597,0.055-0.879,0.16L0.678,19.03
-      C-0.225,19.36-0.228,20.637,0.677,20.977z" transform="translate(64,30) rotate(90 0 0)" filter="url(#f1)" />
-          </g>
-        </svg>
+      C-0.225,19.36-0.228,20.637,0.677,20.977z'
+                  transform='translate(64,30) rotate(90 0 0)'
+                  filter='url(#f1)'
+                />
+              </g>
+            </svg>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-    <div className="cover">
-    <h1><svg className ="icon" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
-<path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
-<path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-</svg>
-         AirTime</h1>
-      <h2>The sky is waiting for you.</h2>
-      <form className="flex-form">
-        {/* <input type="search" placeholder="  Which Airport are you going to?" /> */}
-        <Autocomplete onLoad={onLoad}
-           onPlaceChanged={onPlaceChanged}
-           
-           >
+      <div className='cover'>
+        <h1>
+          <svg
+            className='icon'
+            xmlns='http://www.w3.org/2000/svg'
+            width='50'
+            height='50'
+            fill='currentColor'
+            class='bi bi-geo-alt'
+            viewBox='0 0 16 16'
+          >
+            <path d='M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z' />
+            <path d='M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z' />
+          </svg>
+          AirTime
+        </h1>
+        <h2>The sky is waiting for you.</h2>
+        <form className='flex-form'>
+          {/* <input type="search" placeholder="  Which Airport are you going to?" /> */}
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
             <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
-              <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+              <InputBase
+                placeholder='Search…'
+                classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              />
             </div>
           </Autocomplete>
-        <button type="submit" defaultValue="Search"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-</svg></button>
-      </form>
-      
+          <button type='submit' defaultValue='Search'>
+            {' '}
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='16'
+              height='16'
+              fill='currentColor'
+              class='bi bi-search'
+              viewBox='0 0 16 16'
+            >
+              <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
+            </svg>
+          </button>
+        </form>
+      </div>
     </div>
-  </div>
   );
 }
 
--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -12,9 +12,7 @@ import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import '/Users/sagalafrah/lighthouse/w11/AirTimeApp/client/src/SignIn.css'
-
-
+import '../SignIn.css';
 
 const theme = createTheme();
 
@@ -31,7 +29,7 @@ export default function SignIn() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
+      <Grid container component='main' sx={{ height: '100vh' }}>
         <CssBaseline />
         <Grid
           item
@@ -39,7 +37,6 @@ export default function SignIn() {
           sm={4}
           md={7}
           sx={{
-            
             // backgroundImage: 'url(https://source.unsplash.com/random)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: '#912BF6',
@@ -60,55 +57,59 @@ export default function SignIn() {
             <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
               <LockOutlinedIcon />
             </Avatar>
-            <Typography component="h1" variant="h5">
+            <Typography component='h1' variant='h5'>
               Sign in
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <Box
+              component='form'
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
               <TextField
-                margin="normal"
+                margin='normal'
                 required
                 fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
+                id='email'
+                label='Email Address'
+                name='email'
+                autoComplete='email'
                 autoFocus
               />
               <TextField
-                margin="normal"
+                margin='normal'
                 required
                 fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
+                name='password'
+                label='Password'
+                type='password'
+                id='password'
+                autoComplete='current-password'
               />
               <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
+                control={<Checkbox value='remember' color='primary' />}
+                label='Remember me'
               />
               <Button
-                type="submit"
+                type='submit'
                 fullWidth
-                variant="contained"
+                variant='contained'
                 sx={{ mt: 3, mb: 2 }}
               >
                 Sign In
               </Button>
               <Grid container>
                 <Grid item xs>
-                  <Link href="#" variant="body2">
+                  <Link href='#' variant='body2'>
                     Forgot password?
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link href="/register" variant="body2">
+                  <Link href='/register' variant='body2'>
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
               </Grid>
-              
             </Box>
           </Box>
         </Grid>
