0deb59612331e5898a9257b9ada5e761aeddf335
Ansaar Ahmed <ansaar.ahmed245@gmail.com>
2022-01-20 20:56:09 -0500

    place details rendered on page, name, rating, image displayed as well as link to website and trip advisor page

--- client/src/components/PlaceDetails/PlaceDetails.js
+++ client/src/components/PlaceDetails/PlaceDetails.js
@@ -4,7 +4,7 @@ import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PhoneIcon from '@material-ui/icons/Phone';
 import Rating from '@material-ui/lab/Rating';
 
-import useStyles from './style';
+import useStyles from './style'
 
 const PlaceDetails = ({ place, selected, refProp }) => {
   if (selected) refProp?.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
@@ -23,24 +23,9 @@ const PlaceDetails = ({ place, selected, refProp }) => {
           <Rating name="read-only" value={Number(place.rating)} readOnly />
           <Typography component="legend">{place.num_reviews} review{place.num_reviews > 1 && 's'}</Typography>
         </Box>
-        <Box display="flex" justifyContent="space-between">
-          <Typography component="legend">Price</Typography>
-          <Typography gutterBottom variant="subtitle1">
-            {place.price_level}
-          </Typography>
-        </Box>
-        <Box display="flex" justifyContent="space-between">
-          <Typography component="legend">Ranking</Typography>
-          <Typography gutterBottom variant="subtitle1">
-            {place.ranking}
-          </Typography>
-        </Box>
-        {place?.awards?.map((award) => (
-          <Box display="flex" justifyContent="space-between" my={1} alignItems="center">
-            <img src={award.images.small} />
-            <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
-          </Box>
-        ))}
+        
+       
+        
         {place?.cuisine?.map(({ name }) => (
           <Chip key={name} size="small" label={name} className={classes.chip} />
         ))}
