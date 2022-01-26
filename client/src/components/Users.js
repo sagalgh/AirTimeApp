a32708bf7dc860efc70a1e3e940424a9d32b82a5
Sagal Afrah <afrahsagal@gmail.com>
2022-01-26 03:25:49 -0500

    ready for rehearsal

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -2,7 +2,7 @@ import React from 'react'
 import User from './User'
 const Users = (props) => {
   return (
-    <div style={{width: '5vw'}} className="userList-container col">
+    <div className="userList-container col">
             <div className="userList">
                 <div id="userlist-title" className="row center">
                     <p>Users</p>
